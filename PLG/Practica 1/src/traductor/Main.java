80dcd8e99fc71a9f229754574a7450c5e67b30dc
honguito42 <honguito42@e1185e86-e872-11dd-ab07-b19b4dad1b3d>
2009-01-26 23:02:28 +0000

    Prueba exhaustiva

--- PLG/Practica 1/src/traductor/Main.java
+++ PLG/Practica 1/src/traductor/Main.java
@@ -2,7 +2,9 @@ package traductor;
 
 import interprete.MaquinaEjecucion;
 import interprete.OperandoNum;
+import interprete.OperandoNumReal;
 import interprete.OperandoValorBoolean;
+import interprete.OperandoValorChar;
 import interprete.Operandos;
 import interprete.TokenMaquina;
 
@@ -32,9 +34,29 @@ public class Main {
 		Object dir = new OperandoNum(0);
 		Object escribe = new Operaciones(TokenMaquina.ESCRITURA);
 		Object dir2 = new OperandoNum(0);
-		//Object lee = new Operaciones(TokenMaquina.LECTURA);
-		//Object dir3 = new OperandoNum(0);
-		Object fin = new Operaciones(TokenMaquina.STOP);
+		Object lee = new Operaciones(TokenMaquina.LECTURA);
+		Object dir3 = new OperandoNum(0);
+		Object escribe2 = new Operaciones(TokenMaquina.ESCRITURA);
+		Object dir22 = new OperandoNum(0);		
+		Object e1=new Operaciones(TokenMaquina.APILA);
+		Object c1=new OperandoValorChar('x');
+		Object ap1 = new Operaciones(TokenMaquina.DESAPILA_DIR);
+		Object dir1 = new OperandoNum(1);
+		Object leeChar = new Operaciones(TokenMaquina.LECTURA);
+		Object dir12 = new OperandoNum(1);
+		Object escribe3 = new Operaciones(TokenMaquina.ESCRITURA);
+		Object dir23 = new OperandoNum(1);
+		
+		Object e11=new Operaciones(TokenMaquina.APILA);
+		Object c11=new OperandoNumReal(3.45);
+		Object ap11 = new Operaciones(TokenMaquina.DESAPILA_DIR);
+		Object dir11 = new OperandoNum(2);
+		Object leereal = new Operaciones(TokenMaquina.LECTURA);
+		Object dir121 = new OperandoNum(2);
+		Object escribe31 = new Operaciones(TokenMaquina.ESCRITURA);
+		Object dir231 = new OperandoNum(2);
+		
+		Object fin = new Operaciones(TokenMaquina.STOP);	
 
 		obj.add(e);
 		obj.add(c);
@@ -44,9 +66,27 @@ public class Main {
 		obj.add(ap);
 		obj.add(dir);
 		obj.add(escribe);
-		obj.add(dir2);
-		//obj.add(lee);
-		//obj.add(dir3);		
+		obj.add(dir2);	
+		obj.add(lee);
+		obj.add(dir3);
+		obj.add(escribe2);
+		obj.add(dir22);	
+		obj.add(e1);
+		obj.add(c1);
+		obj.add(ap1);
+		obj.add(dir1);
+		obj.add(leeChar);
+		obj.add(dir12);
+		obj.add(escribe3);
+		obj.add(dir23);	
+		obj.add(e11);
+		obj.add(c11);
+		obj.add(ap11);
+		obj.add(dir11);
+		obj.add(leereal);
+		obj.add(dir121);
+		obj.add(escribe31);
+		obj.add(dir231);	
 		obj.add(fin);
 		
 		traductor t = new traductor(obj);
