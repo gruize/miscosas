71c4cb2e189a30d262ee271980df876c0f8e1087
honguito42 <honguito42@e1185e86-e872-11dd-ab07-b19b4dad1b3d>
2009-01-27 18:43:24 +0000

    comentado

--- PLG/Practica 1/src/sintactico/tablaSimbolos/TablaSimbolo.java
+++ PLG/Practica 1/src/sintactico/tablaSimbolos/TablaSimbolo.java
@@ -8,13 +8,20 @@ public class TablaSimbolo {
   public Hashtable<String, DatosTabla> ht;
   public int dirAct;
 
- 
+  /**
+   * crea una tabla de simbolos
+   */
   public void creaTS()
 	{
 		ht= new Hashtable<String, DatosTabla>();
 		dirAct=0;
 	}
-  
+  /**
+   * Añade un id a la tabla
+   * @param id identificador del elemento
+   * @param tipo_de_DEC tipo del identificador
+   * @param modificable indica si es variable o constante
+   */
   public void anadeID (String id, int tipo_de_DEC, boolean modificable)
 	{
 	  	
@@ -25,7 +32,11 @@ public class TablaSimbolo {
 		ht.put(id, datos);
 		dirAct++;
 	}
-  
+  /**
+   * Devuelve la direccion en la que esta almacenada el id
+   * @param id 
+   * @return direccion donde almacenada id
+   */
   public int dameDir(String id)  {
 	  DatosTabla datos=(DatosTabla) ht.get(id);
 	  return datos.dameDir();
@@ -33,18 +44,31 @@ public class TablaSimbolo {
 	  
   }
   
+  /**
+   * Devuelve el tipo de id
+   * @param id 
+   * @return tipo id
+   */
   public int dameTipo(String id){
 	  DatosTabla datos=(DatosTabla) ht.get(id);
 	  return datos.dameTipo();
 	  
   }
-  
+  /**
+   * Devuelve si el ide es variable o constante id
+   * @param id 
+   * @return true si variable false si constante
+   */
   public boolean dameModificable(String id){
 	  DatosTabla datos=(DatosTabla) ht.get(id);
 	  return datos.dameModificable();
 	  
   }
-  
+  /**
+   * Devuelve si existe ya el id en la tabla
+   * @param id 
+   * @return true si existe el id false eoc
+   */
   public boolean existeID(String ID){
 	  return ht.containsKey(ID);
   }
